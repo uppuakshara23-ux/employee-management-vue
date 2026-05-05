e5fc6fae92d2ad0bd165c45328b5ad7e4b408fb1
uppuakshara23-ux <uppuakshara23@gmail.com>
2026-05-05 21:52:58 +0530

    fix vite base path and deploy

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/employee-management-vue/',   // ⭐ THIS LINE IS IMPORTANT
   plugins: [
     vue(),
     vueDevTools(),
