850b69b7282ab87f7230ec821f61a9b28d16a0d0
uppuakshara23-ux <uppuakshara23@gmail.com>
2026-05-05 21:57:52 +0530

    fix github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/employee-management-vue/',   // ⭐ THIS LINE IS IMPORTANT
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/employee-management-vue/',   // 🔴 MUST MATCH REPO NAME
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
